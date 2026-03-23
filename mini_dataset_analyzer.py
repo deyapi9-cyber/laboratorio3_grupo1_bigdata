8b8304a1c65ec00bcfaed28971da89c04b092026
deyapi9-cyber <deyapi9@gmail.com>
2026-03-22 20:56:49 -0500

    Se actualiza código con las validaciones y los docstring

--- mini_dataset_analyzer.py
+++ mini_dataset_analyzer.py
@@ -1,23 +1,45 @@
-datos = []
+"""
+Módulo que contiene el programa mini_dataset_analyzer.
+Permite analizar un conjunto de datos ingresados por el usuario.
+"""
 
-cantidad = int(input("¿Cuántos datos desea ingresar?: "))
 
-for i in range(cantidad):
-    numero = float(input(f"Ingrese el dato #{i + 1}: "))
-    datos.append(numero)
+def mini_dataset_analyzer():
+    """
+    Esta función permite ingresar un conjunto de datos numéricos
+    y calcula el promedio, valor máximo, valor mínimo y
+    la cantidad de registros.
+    """
 
-suma = 0
+    datos = []
 
-for valor in datos:
-    suma += valor
+    cantidad = int(input("¿Cuántos datos desea ingresar?: "))
 
-promedio = suma / len(datos)
-maximo = max(datos)
-minimo = min(datos)
-cantidad_registros = len(datos)
+    for i in range(cantidad):
+        numero = float(input(f"Ingrese el dato #{i + 1}: "))
+        datos.append(numero)
 
-print("\n--- RESULTADOS DEL DATASET ---")
-print("Promedio:", promedio)
-print("Valor máximo:", maximo)
-print("Valor mínimo:", minimo)
-print("Cantidad de registros:", cantidad_registros)
+    if len(datos) > 0:
+        suma = 0
+
+        for valor in datos:
+            suma += valor
+
+        promedio = suma / len(datos)
+        maximo = max(datos)
+        minimo = min(datos)
+        cantidad_registros = len(datos)
+    else:
+        promedio = 0
+        maximo = 0
+        minimo = 0
+        cantidad_registros = 0
+
+    print("\n--- RESULTADOS DEL DATASET ---")
+    print("Promedio:", promedio)
+    print("Valor máximo:", maximo)
+    print("Valor mínimo:", minimo)
+    print("Cantidad de registros:", cantidad_registros)
+
+
+mini_dataset_analyzer()
