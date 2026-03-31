e3169c505763596ec7df266b63ebe9ce4b0495eb
jzamorag1985-web <jzamorag1985@gmail.com>
2026-03-31 18:27:28 -0500

    codigo #1 mass_sales_analyzer con variables definidas y se realizo validacion

--- mass_sales_analyzer.py
+++ mass_sales_analyzer.py
@@ -1,46 +1,49 @@
 # 1 Project mass_sales_analyzer
 
-# Project mass_sales_analyzer
 
-sales = []
+def mass_sales_analyzer():
 
-sales_count = int(input("¿Cuántas ventas desea registrar?: "))
+    sales = []
 
-for i in range(sales_count):
-    print("\nVenta", i + 1)
+    sales_count = int(input("¿Cuántas ventas desea registrar?: "))
+
+    for i in range(sales_count):
+        print("\nVenta", i + 1)
     
-    product = input("Ingrese el nombre del producto: ")
-    price = float(input("Ingrese el precio del producto: "))
-    quantity = int(input("Ingrese la cantidad vendida: "))
+        product = input("Ingrese el nombre del producto: ")
+        price = float(input("Ingrese el precio del producto: "))
+        quantity = int(input("Ingrese la cantidad vendida: "))
 
-    sale = {
-        "product": product,
-        "price": price,
-        "quantity": quantity,
-        "total": price * quantity
-    }
+        sale = {
+            "product": product,
+            "price": price,
+            "quantity": quantity,
+            "total": price * quantity
+        }
 
-    sales.append(sale)
+        sales.append(sale)
 
-total_sales = 0
-largest_sale = sales[0]
+    total_sales = 0
+    largest_sale = sales[0]
 
-for sale in sales:
-    total_sales += sale["total"]
+    for sale in sales:
+        total_sales += sale["total"]
 
-    if sale["total"] > largest_sale["total"]:
-        largest_sale = sale
+        if sale["total"] > largest_sale["total"]:
+            largest_sale = sale
 
-average_sales = total_sales / len(sales)
+    average_sales = total_sales / len(sales)
 
-print("\n--- RESULTADOS ---")
+    print("\n--- RESULTADOS ---")
 
-option = "results"
+    option = "results"
 
-match option:
-    case "results":
-        print("Total vendido:", total_sales)
-        print("Promedio de ventas:", average_sales)
-        print("Venta más grande:")
-        print("Producto:", largest_sale["product"])
-        print("Total:", largest_sale["total"])
+    match option:
+        case "results":
+            print("Total vendido:", total_sales)
+            print("Promedio de ventas:", average_sales)
+            print("Venta más grande:")
+            print("Producto:", largest_sale["product"])
+            print("Total:", largest_sale["total"])
+        
+mass_sales_analyzer()        
